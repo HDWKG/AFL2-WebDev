2ec95f0348e3daad12a5cbb4bcf2f66a97aabd27
Daniel Fernando Herawan <dfernando@student.ciputra.ac.id>
2023-11-06 10:44:26 +0700

    Update Product.php

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -10,5 +10,5 @@ class Product extends Model
 
     protected $table = 'products';
 
-    protected $fillable = ['name', 'desc', 'price', 'image', 'category_id'];
+    protected $fillable = ['category_id', 'name', 'desc', 'price', 'image'];
 }
